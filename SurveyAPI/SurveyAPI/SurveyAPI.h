a6aef86bbf59e28210b6394f5266fa6f4e4cb685
Dominik Babić <domynick93@hotmail.com>
2024-03-25 11:24:30 +0100

    Adds SurveyAPI module.

---
+++ SurveyAPI/SurveyAPI/SurveyAPI.h
@@ -0,0 +1,18 @@
+//
+//  SurveyAPI.h
+//  SurveyAPI
+//
+//  Created by Dominik Babic on 25.03.2024..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SurveyAPI.
+FOUNDATION_EXPORT double SurveyAPIVersionNumber;
+
+//! Project version string for SurveyAPI.
+FOUNDATION_EXPORT const unsigned char SurveyAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SurveyAPI/PublicHeader.h>
+
+
